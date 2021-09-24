5e3100e753adeedbaa397c3864b7516ddab488ac
steelydylan <ess_president@me.com>
2021-09-24 21:38:08 +0900

    fix: script trivial fix

---
+++ pages/_document.tsx
@@ -0,0 +1,47 @@
+import blogConfig from "@/blog.config";
+import Script from "next/script";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document<{
+  styleTags: JSX.Element;
+}> {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {blogConfig.googleAnalyticsCode && (
+            <>
+              <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${blogConfig.googleAnalyticsCode}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${blogConfig.googleAnalyticsCode}');`,
+                }}
+              />
+            </>
+          )}
+          {blogConfig.googleAdsenseCode && (
+            <>
+              <Script
+                strategy="afterInteractive"
+                src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${blogConfig.googleAdsenseCode}"
+              crossOrigin="anonymous`}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
