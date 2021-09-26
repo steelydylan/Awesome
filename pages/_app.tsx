572b97731eeecaf723d2e5930ba200e5df5106e8
steelydylan <ess_president@me.com>
2021-09-26 15:57:38 +0900

    fix: trivial fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { GlobalStyle } from "@/styles";
 import {
   faFacebook,
@@ -34,6 +35,32 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      {blogConfig.googleAnalyticsCode && (
+        <>
+          <Script
+            defer
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${blogConfig.googleAnalyticsCode}`}
+          />
+          <Script defer strategy="afterInteractive">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${blogConfig.googleAnalyticsCode}');
+            `}
+          </Script>
+        </>
+      )}
+      {blogConfig.googleAdsenseCode && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${blogConfig.googleAdsenseCode}"
+              crossOrigin="anonymous`}
+          />
+        </>
+      )}
       <GlobalStyle />
       <Component {...pageProps} />
     </>
