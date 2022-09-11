08c119ce3a9ecff75bbc50213af1e8d6237be233
steelydylan <ess_president@me.com>
2022-09-11 12:48:31 +0900

    fix

--- blog.config.ts
+++ blog.config.ts
@@ -1,7 +1,7 @@
 import { BlogConfig } from "./types/config";
 
 const blogConfig: BlogConfig = {
-  use: "notion",
+  use: "mdx", // mdx or notion
   siteLogo: {
     url: "/images/logo.svg",
     width: "201",
